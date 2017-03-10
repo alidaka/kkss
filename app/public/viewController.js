c54e54c2893d0a70a78b839fd08f5e557f3db4ff
Augustus Lidaka <alidaka@pivotal.io>
2017-03-10 11:37:58 -0800

    minor viewController and test method extraction/refactors

--- app/public/viewController.js
+++ app/public/viewController.js
@@ -4,30 +4,36 @@
     this.generator = generator;
     this.output = output;
     this.input = input;
+
     this.input.addEventListener("submit", this.submitHandler.bind(this));
+  };
+
+  KKSS.viewController.prototype._viewForKeys = function(keys, minimumParts) {
+    var list = document.createElement("ul");
+    for (var i = 0; i < keys.length; i++) {
+      var part = document.createElement("li");
+      part.innerText = [minimumParts, keys[i][0], keys[i][1]].join('-');
+      list.appendChild(part);
+    }
+
+    return list;
   };
 
   KKSS.viewController.prototype.submitHandler = function(event) {
     event.preventDefault();
+
     var secret = this.input.querySelector("[name=secret]").value;
     var totalParts = parseInt(this.input.querySelector("[name=total-parts]").value);
     var minimumParts = parseInt(this.input.querySelector("[name=minimum-parts]").value);
+    var keys = this.generator.decompose(secret, minimumParts, totalParts);
 
-    var partialKeys = this.generator.decompose(secret, minimumParts, totalParts);
+    var view = this._viewForKeys(keys, minimumParts);
 
-    var list = document.createElement("ul");
-    for (var i = 0; i < partialKeys.length; i++) {
-      var part = document.createElement("li");
-      part.innerText = [minimumParts, partialKeys[i][0], partialKeys[i][1]].join('-');
-      list.appendChild(part);
-    }
-
-    if (this.output.firstChild)
-    {
+    if (this.output.firstChild) {
         this.output.removeChild(this.output.firstChild);
     }
 
-    this.output.appendChild(list);
+    this.output.appendChild(view);
 
     return false;
   };
--- spec/client/viewControllerSpec.js
+++ spec/client/viewControllerSpec.js
@@ -14,31 +14,35 @@ describe("ViewController", function() {
     };
 
     this.subject = new KKSS.viewController(this.inputForm, this.outputDiv, this.mockGenerator);
+
+    this.submitForm = function() {
+      var evt = new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window
+      });
+      var button = this.inputForm.querySelector("[name=generate-button]");
+      button.dispatchEvent(evt);
+    };
   });
 
   it("decomposes the secret when the submit button is clicked", function() {
-    var evt = new MouseEvent("click", {
-      bubbles: true,
-      cancelable: true,
-      view: window
-    });
-    var button = this.inputForm.querySelector("[name=generate-button]");
-    button.dispatchEvent(evt);
-
+    this.submitForm();
     expect(this.mockGenerator.decompose).toHaveBeenCalledWith("hello", 2, 3);
   });
 
   it("prints out the result of decomposing the secret", function() {
-    var evt = new MouseEvent("click", {
-      bubbles: true,
-      cancelable: true,
-      view: window
-    });
-    var button = this.inputForm.querySelector("[name=generate-button]");
-    button.dispatchEvent(evt);
-
+    this.submitForm();
     expect(this.outputDiv.innerHTML).toContain("2-1-123123123");
     expect(this.outputDiv.innerHTML).toContain("2-2-123123124");
     expect(this.outputDiv.innerHTML).toContain("2-3-123123125");
+  });
+
+  it("replaces partial displays from previous calls", function() {
+    var staleContent = document.createElement("ul");
+    this.outputDiv.appendChild(staleContent);
+
+    this.submitForm();
+    expect(this.outputDiv.children).not.toContain(staleContent);
   });
 });
